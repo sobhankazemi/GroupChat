d7ddbe99804e18da82592e0fa6a712d7b9e3db4b
sobhankazemi <sobhankazemi80@gmail.com>
2023-05-12 00:02:01 +0330

    updating run script

--- run.sh
+++ run.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-docker container  run -d --hostname rabbit --name rabbit1 -p 15672:15672 -p 5672:5672 rabbitmq:3-management
-cd Chat
-go run *.go &
-cd ../History
-go run *.go &
+docker compose build
+docker compose up -d
